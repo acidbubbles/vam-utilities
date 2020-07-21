92de03344eaa75c0d518d8779bca4cf84faffdda
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-07-21 09:22:35 -0400

    Add far clip plane

--- CameraNearClipPlane.cs
+++ CameraNearClipPlane.cs
@@ -10,39 +10,55 @@ public class CameraNearClipPlane : MVRScript
 {
     private Camera _mainCamera;
     private JSONStorableFloat _nearClipPlaneJSON;
-    private float _originalClipDistance;
+    private JSONStorableFloat _farClipPlaneJSON;
+    private float _originalNearClipDistance;
+    private float _originalFarClipDistance;
 
     public override void Init()
     {
         _mainCamera = CameraTarget.centerTarget?.targetCamera;
-        _originalClipDistance = _mainCamera.nearClipPlane;
+        _originalNearClipDistance = _mainCamera.nearClipPlane;
+        _originalFarClipDistance = _mainCamera.farClipPlane;
 
         CreateTextField(new JSONStorableString("Warning", "Warning: If you increase the range, you could lose the menu and be forced to restart Virt-A-Mate.")).enabled = false;
+
         _nearClipPlaneJSON = new JSONStorableFloat("Near clip plane", 0.01f, val => SyncCameraClipping(val), 0.01f, 1.5f, false);
         RegisterFloat(_nearClipPlaneJSON);
         CreateSlider(_nearClipPlaneJSON);
 
-        SyncCameraClipping(_nearClipPlaneJSON.val);
+        _farClipPlaneJSON = new JSONStorableFloat("Far clip plane", 0.01f, val => SyncCameraClipping(val), 0.01f, 1.5f, false);
+        RegisterFloat(_farClipPlaneJSON);
+        CreateSlider(_farClipPlaneJSON);
+
+        SyncCameraClipping();
     }
 
     public void OnEnable()
     {
-        SyncCameraClipping(_nearClipPlaneJSON.val);
+        SyncCameraClipping();
     }
 
     public void OnDisable()
     {
-        _mainCamera.nearClipPlane = _originalClipDistance;
+        _mainCamera.nearClipPlane = _originalNearClipDistance;
+        _mainCamera.farClipPlane = _originalFarClipDistance;
     }
 
-    private void SyncCameraClipping(float nearClipPlane)
+    private void SyncCameraClipping()
     {
-        if (nearClipPlane <= 0f)
+        if (_nearClipPlaneJSON.val <= 0f || _farClipPlaneJSON.val <= 0)
         {
             SuperController.LogError("Cannot set the clip distance to 0 or less");
             return;
         }
 
-        _mainCamera.nearClipPlane = nearClipPlane;
+        if (_nearClipPlaneJSON.val > _farClipPlaneJSON.val)
+        {
+            SuperController.LogError("Cannot set the far clip distance to 0 or less");
+            return;
+        }
+
+        _mainCamera.nearClipPlane = _nearClipPlaneJSON.val;
+        _mainCamera.farClipPlane = _farClipPlaneJSON.val;
     }
 }
