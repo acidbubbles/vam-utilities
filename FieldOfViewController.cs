dfecb057706ed25c5f0cb66426ef868ec20756cd
everlaster <everlaster@protonmail.com>
2024-02-05 23:17:42 +0200

    FieldOfViewController: if plugin is in session, fov isStorable and VAM's FOV slider value syncs back to plugin

--- FieldOfViewController.cs
+++ FieldOfViewController.cs
@@ -10,18 +10,21 @@ public class FieldOfViewController : MVRScript
 {
     public override void Init()
     {
+        bool isSessionPlugin = containingAtom.type == "SessionPluginManager";
         var slider = SuperController.singleton.monitorCameraFOVSlider;
 
         var monitorFOVJSON = new JSONStorableFloat("Monitor FoV", 40f, slider.minValue, slider.maxValue)
         {
-            isStorable = false,
+            isStorable = isSessionPlugin,
             valNoCallback = slider.value,
-            setCallbackFunction = val =>
-            {
-                slider.value = val;
-            }
+            setCallbackFunction = val => slider.value = val
         };
         RegisterFloat(monitorFOVJSON);
         CreateSlider(monitorFOVJSON).label = "Monitor FoV (Set Only)";
+
+        if(isSessionPlugin)
+        {
+            slider.onValueChanged.AddListener(val => monitorFOVJSON.valNoCallback = val);
+        }
     }
 }
